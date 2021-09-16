bcf007aeb48e9600735a2e63e012d606ca88d02c
Cheng Ye <chye@ucsd.edu>
2021-09-16 20:01:14 +0000

    variable runtime

--- benchmark_matOptimize.sh
+++ benchmark_matOptimize.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/bash
+TIME=24
+if [ $# -eq 2 ]
+then
+	TIME=$2
+fi
 set -e
 TERM=linux
 cd $(dirname $0)
@@ -6,4 +11,4 @@ shopt -s expand_aliases
 alias time="/usr/bin/time -f '\n%e Elapsed\n%Mk resident\n%U user\n%S sys'"
 VCF_PATH=$1/init.vcf.gz
 TREE_PATH=$1/init.nwk
-{ time matOptimize -t $TREE_PATH -v $VCF_PATH -o $1/matOptimize_out.pb -n -M 24; } &> $1/matOptimize_log
+{ time matOptimize -t $TREE_PATH -v $VCF_PATH -o $1/matOptimize_out.pb -n -M $TIME; } &> $1/matOptimize_log
